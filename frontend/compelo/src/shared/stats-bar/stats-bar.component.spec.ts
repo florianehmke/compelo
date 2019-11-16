295f7c7beb83b4b7d596be04371341cb3b616b41
Florian Ehmke <ehmkeflorian@gmail.com>
2019-11-16 12:44:03 +0100

    test(stats-bar): added test

--- frontend/compelo/src/shared/stats-bar/stats-bar.component.spec.ts
+++ frontend/compelo/src/shared/stats-bar/stats-bar.component.spec.ts
@@ -1,24 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 
 import { StatsBarComponent } from './stats-bar.component';
 
 describe('StatsBarComponent', () => {
-  let component: StatsBarComponent;
-  let fixture: ComponentFixture<StatsBarComponent>;
+  let spectator: Spectator<StatsBarComponent>;
+  const createComponent = createComponentFactory(StatsBarComponent);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [StatsBarComponent]
-    }).compileComponents();
-  }));
+  beforeEach(() => (spectator = createComponent()));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StatsBarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should not render segments if data input is null', () => {
+    expect(spectator.query('.segment')).toBeNull();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render segments according to the input data', () => {
+    spectator.setInput('data', {
+      wins: 3,
+      draws: 2,
+      lost: 1
+    });
+
+    expect(spectator.query('.segment.wins')).toHaveText('3');
+    expect(spectator.query('.segment.draws')).toHaveText('2');
+    expect(spectator.query('.segment.lost')).toHaveText('1');
   });
 });
