3fcdada84ac440de3700dc2a8cf4b9bed9905df9
Florian Ehmke <ehmkeflorian@gmail.com>
2024-11-07 18:52:07 +0100

    chore: post ngrx router store update

--- frontend/compelo/src/core/router/router-state.selectors.ts
+++ frontend/compelo/src/core/router/router-state.selectors.ts
@@ -1,16 +1,11 @@
-import * as fromRouter from '@ngrx/router-store';
+import { getRouterSelectors, RouterReducerState } from '@ngrx/router-store';
 import { createFeatureSelector } from '@ngrx/store';
 
 import { gameIdParam, projectIdParam } from '@shared/route-params';
 
-import { FEATURE_KEY, State } from './router-state.reducer';
+export const selectRouter = createFeatureSelector<RouterReducerState<never>>('router');
 
-export const selectRouter = createFeatureSelector<
-  State,
-  fromRouter.RouterReducerState<any>
->(FEATURE_KEY);
-
-const { selectRouteParam } = fromRouter.getSelectors(selectRouter);
+const { selectRouteParam } = getRouterSelectors(selectRouter);
 
 export const getSelectedGameId = selectRouteParam(gameIdParam);
 export const getSelectedProjectId = selectRouteParam(projectIdParam);
